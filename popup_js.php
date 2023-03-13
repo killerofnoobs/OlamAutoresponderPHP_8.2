5b07a60a61c492348a02d1d53c8a3a54e4e65c94
AlmostSavage <127709055+AlmostSavage@users.noreply.github.com>
2023-03-13 16:06:56 -0500

    Update popup_js.php

--- popup_js.php
+++ popup_js.php
@@ -1,6 +1,10 @@
+// edit 2023 CW
+// no changes to 8.2
+
 <script language="javascript" type="text/javascript">
 <!--
 function popper(url) {
+	// declares popup window
 	newwindow=window.open(url,'popper','height=500,width=600,scrollbars=1');
 	if (window.focus) {newwindow.focus()}
 	return false;
